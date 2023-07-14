7cf93e8f5a7d50d36295f70ace7e2527414c7e9f
Giovanna Oliveira <107190603+ansiee@users.noreply.github.com>
2023-07-14 12:33:47 -0300

    Add files via upload

---
+++ AV GIOVANNA/index.js
@@ -0,0 +1,65 @@
+// const http = require('node:http')
+// const { totalmem } = require('node:os')
+// const { prototype } = require('events')
+// const path = require('path')
+
+
+
+// Atividade 01
+
+// http.get({
+//     nomeSistemaOperacional: '',
+//     memoriaTotal: '',
+// }
+// )
+
+
+
+// Atividade 02
+
+// path.extname('https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf')
+// console.log(path.extname)
+
+// path.parse('https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf')
+// console.log(path.parse)
+
+
+
+// Atividade 03
+
+// const url = {
+//     protocol: 'https:',
+//     slashes: 'true',
+//     auth: null,
+//     hostname: 'nodejs.org',
+//     hash:'#url_new_url_input_base',
+//     search: null,
+//     query: [Object: null prototype] {},
+//     pathname: '/api/url.html',
+//     path: '/api/url.html',
+//     href: 'https://nodejs.org/api/url.html#url_new_url_input_base'
+// }
+
+// console.log(url.protocol)
+// // console.log(url.slashes)
+// console.log(url.auth)
+// console.log(url.hostname)
+// console.log(url.hash)
+// console.log(url.search)
+// console.log(url.pathname)
+// console.log(url.path)
+// console.log(url.href)
+
+
+
+// Atividade 04
+// import { rename } from 'node:fs';
+
+// rename('teste.txt', 'testerenomeado.txt', (err) => {
+//   if (err) throw err;
+//   console.log('Rename complete!');
+// }); 
+
+
+//Atividade 05
+
